4a36a1250357b169c4893ca8113bbb631ca2b308
colani-sudo <matsenjwacolani@gmail.com>
2023-06-27 10:22:52 +0800

    initial commit

---
+++ adversary.py
@@ -0,0 +1,15 @@
+# from string import digits
+
+# for i in digits:
+#     for j in digits:
+#         for k in digits:
+#             for l in digits:
+#                 print(i + j + k + l)
+
+from string import ascii_letters, digits, punctuation
+
+for i in ascii_letters + digits + punctuation:
+    for j in ascii_letters + digits + punctuation:
+        for k in ascii_letters + digits + punctuation:
+            for l in ascii_letters + digits + punctuation:
+                print(i + j + k + l)
